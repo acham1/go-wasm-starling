8ed9eb383e794fecdb686045b76f72d7d9f9b3e3
acham1 <alanchamis@gmail.com>
2020-10-19 03:23:40 -0700

    handle concurrency

--- main.go
+++ main.go
@@ -22,11 +22,11 @@ func resizeBounds(this js.Value, inputs []js.Value) interface{} {
 		width  = float64(inputs[0].Float())
 		height = float64(inputs[1].Float())
 	)
-	resizeCanvas(width, height)
+	resizeBoundsFloat64(width, height)
 	return nil
 }
 
-func resizeCanvas(width, height float64) {
+func resizeBoundsFloat64(width, height float64) {
 	WindowWidth = width
 	WindowHeight = height
 	val := js.Global().Get("document").Call("getElementById", "canvas")
@@ -35,13 +35,11 @@ func resizeCanvas(width, height float64) {
 
 func main() {
 	js.Global().Set("resizeBounds", js.FuncOf(resizeBounds))
+	resizeBoundsFloat64(0, 0)
 
 	go func() {
 		for radius := 10; radius < 1000; radius++ {
 			time.Sleep(time.Second)
-			if Renderer == nil {
-				continue
-			}
 			ctx := canvas.NewContext(Renderer)
 			ctx.DrawPath(500, 250, canvas.Circle(float64(radius)))
 		}
